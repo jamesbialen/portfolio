3c3525137aa2a096ac68abd53f9f7bbe0df361ea
James Bialen <116704205+jamesbialen@users.noreply.github.com>
2023-04-29 11:51:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function displayClock() {
+        var display = document.getElementById("clock");
+        var time = new Date();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        var ampm = hours >= 12 ? "PM" : "AM";
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        var currentTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
+        display.innerHTML = currentTime;
+        setTimeout(displayClock, 1000);
+      }
+   
+
 const hamburger = document.getElementsByClassName("hamburger")[0]
 const mobileNavs = document.getElementsByClassName("nav-links")[0]
 hamburger.addEventListener("click", () => {
